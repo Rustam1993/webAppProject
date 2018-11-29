6738eb0ced34cbfb0d36bed62f2d5b7dda98d2a9
Rustam_Samigullin <rstsmg1@gmail.com>
2018-11-29 15:06:54 -0500

    design for 600px screen

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,15 @@
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+  } else {
+      x.className = "topnav";
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 
+  
   console.log('IronGenerator JS imported successfully!');
 
 }, false);
